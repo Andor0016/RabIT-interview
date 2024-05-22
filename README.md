5a34fd58dca85f4535723747f24e9ff470686aca
Balo Andor <balo.andor@xpericad.hu>
2024-05-22 11:13:45 +0200

    first steps

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # RabIT-interview
+
+## 1. Set up environment:
+### To see the project, you need to create the database and modify credentials in databaseCredentials.json
+#### Creating Database
+<p>In resources folder, you can find a markdown file, named Database, you need to create a Database, named "rabitdb".</p>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>  
+    <head>
+        <meta charset="UTF-8">
+        <meta author="Andor Balo">    
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Menu</title>
+        <link rel="stylesheet" href="./style/styles.css">
+    </head> 
+    <body>
+        <main>
+            <h1>Welcome!</h1>
+            <h3>Select from the available options!</h3>
+            <div id="menu">
+                <a href="#">
+                    <p>Users</p>
+                </a>
+                <a href="#">
+                    <p>Advertisments</p>
+                </a>
+            </div>
+        </main>
+    </body>
+</html>
